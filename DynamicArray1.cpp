8fc9f63693c55a290f131ac0b3ce78684021b5be
ALKorzh <alexkorzhwork@gmail.com>
2024-03-12 15:09:43 +0300

    add func fillArray

--- DynamicArray1.cpp
+++ DynamicArray1.cpp
@@ -1,8 +1,29 @@
 ﻿#include <iostream>
+#include <ctime>
 using namespace std;
+
+void fillArray(int n, int* numbers) {
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++) {
+        numbers[i] = rand() % 100;
+        cout << numbers[i] << endl;
+    }
+}
+
+int  findMaxEl() 
+{
+
+} 
 
 int main()
 {
-    cout << "Hello World!\n";
+
+    int n;
+    cout << "Enter length of array:" << endl;
+    cin >> n;
+
+    int* numbers{ new int[n] {} };
+    fillArray(n, numbers);
 }
 
