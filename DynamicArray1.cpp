dcc7cf35fbea885c037b3922c879e4939a28f4ed
ALKorzh <alexkorzhwork@gmail.com>
2024-03-12 15:16:42 +0300

    add func findMaxEl

--- DynamicArray1.cpp
+++ DynamicArray1.cpp
@@ -2,18 +2,25 @@
 #include <ctime>
 using namespace std;
 
-void fillArray(int n, int* numbers) {
+void fillArray(int n, int* array) {
     srand(time(NULL));
 
     for (int i = 0; i < n; i++) {
-        numbers[i] = rand() % 100;
-        cout << numbers[i] << endl;
+        array[i] = rand() % 100;
+        cout << array[i] << endl;
     }
 }
 
-int  findMaxEl() 
+int  findMaxEl(int n,int* array) 
 {
+    int maxNum = -1;
+    for (int i = 0; i < n; i++) {
+        if (maxNum < array[i]) {
+            maxNum = array[i];
+        }
+    }
 
+    return maxNum;
 } 
 
 int main()
@@ -25,5 +32,11 @@ int main()
 
     int* numbers{ new int[n] {} };
     fillArray(n, numbers);
+
+    cout << findMaxEl(n, numbers) << endl;
+
+
+
+    delete[] numbers;
 }
 
